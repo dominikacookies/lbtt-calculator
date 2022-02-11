faca2d662afd70518fea1e9c9383cc94334bd755
Dominika Pietrzak <dom.pietrzak@capgemini.com>
2022-02-11 15:39:45 +0000

    tests: unit tests for lbttCalculator

---
+++ server/tests/unit/calculator.js/calculator.test.js
@@ -0,0 +1,31 @@
+const { lbttCalculator } = require("../../../calculator");
+
+test("returns correct lbtt rate for properties valued less than £145,000", () => {
+  expect(lbttCalculator(145000)).toBe(0);
+});
+
+test("returns correct lbtt rate for properties valued between £145,000.01 and £250,000.00", () => {
+  expect(lbttCalculator(250000)).toBe(5000);
+});
+
+test("returns correct lbtt rate for properties valued between £250,000.01 and £325,000.00", () => {
+  expect(lbttCalculator(325000)).toBe(16250);
+});
+
+test("returns correct lbtt rate for properties valued between £325,000.01 and £750,000.00", () => {
+  expect(lbttCalculator(750000)).toBe(75000);
+});
+
+test("returns correct lbtt rate for properties valued above £750,000.00", () => {
+  expect(lbttCalculator(750001)).toBe(90000.12);
+});
+
+test("throws error if input is not provided", () => {
+  expect(() => lbttCalculator()).toThrow("Please provide a property price.");
+});
+
+test("throws error if input is not of type number", () => {
+  expect(() => {
+    lbttCalculator("hello");
+  }).toThrowError("Please provide a property price of type number.");
+});
